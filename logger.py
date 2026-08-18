45c78d4310130e807e1d38da2def4abaad6659b9
Bence Kovács <genevievedemars_02@outlook.com>
2026-08-18 16:14:44 +0200

    test:logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,29 +1,20 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-class GameLogger:
-    def __init__(self, name='GameLogger', level=logging.INFO):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
-        handler = logging.FileHandler('game.log')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+def setup_logger(log_file='app.log', max_bytes=10485760, backup_count=5):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
-    def log_info(self, msg):
-        self.logger.info(msg)
-
-    def log_warning(self, msg):
-        self.logger.warning(msg)
-
-    def log_error(self, msg):
-        self.logger.error(msg)
-
-    def log_debug(self, msg):
-        self.logger.debug(msg)
+logger = setup_logger()
 
 if __name__ == '__main__':
-    game_logger = GameLogger()
-    game_logger.log_info('Game started')
-    game_logger.log_warning('Low health warning')
-    game_logger.log_error('Player crashed')
-    game_logger.log_debug('State variables initialized')
+    logger.info('Logger setup complete.')
+    logger.debug('This is a debug message.')
+    logger.warning('This is a warning message.')
+    logger.error('This is an error message.')
+    logger.critical('This is critical!')
